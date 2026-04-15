512c32b0cbb031d1e4ccc90c04bfe7e77a74a91d
bOOmkErOk <gagulidzesandro@gmail.com>
2026-04-15 16:07:14 +0400

    security: permanently removed hardcoded credentials

--- E-Commerce.Infrastructure/SMTP/EmailService.cs
+++ E-Commerce.Infrastructure/SMTP/EmailService.cs
@@ -1,4 +1,5 @@
-﻿using E_Commerce.Application.Interfaces.SMTP;
+using E_Commerce.Application.Interfaces.SMTP;
+using Microsoft.Extensions.Configuration;
 using System.Net;
 using System.Net.Mail;
 
@@ -6,23 +7,30 @@ namespace E_Commerce.Infrastructure.SMTP;
 
 public sealed class EmailService : IEmailService
 {
-    private readonly string _email = "codesell.web@gmail.com";
-    private readonly string _password = "psvn zhpr yyau oliu";
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration config)
+    {
+        _config = config;
+    }
 
     public async Task SendEmailAsync(string subject, string body, string email)
     {
+        var senderEmail = _config["EmailSettings:Email"];
+        var appPassword = _config["EmailSettings:Password"];
+
         using var mail = new MailMessage();
-        mail.From = new MailAddress(_email, "E-Commerce");
+        mail.From = new MailAddress(senderEmail!, "E-Commerce");
         mail.IsBodyHtml = true;
         mail.Subject = subject;
         mail.To.Add(email);
         mail.Body = body;
 
-        var smtpClient = new SmtpClient("smtp.gmail.com")
+        using var smtpClient = new SmtpClient("smtp.gmail.com")
         {
             Port = 587,
             EnableSsl = true,
-            Credentials = new NetworkCredential(_email, _password),
+            Credentials = new NetworkCredential(senderEmail, appPassword),
         };
 
         await smtpClient.SendMailAsync(mail);
